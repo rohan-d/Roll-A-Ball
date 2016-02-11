26f14bc1d38d12639231ef8d4986ba49c011e8e2
Rohan D <ioa-999@outlook.com>
2016-02-11 09:17:48 +0100

    10th - added 2nd stage and working teleporter from 1st to 2nd stage

---
+++ Roll-A-Ball/Assets/Scripts/Teleporter1.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Teleporter1 : MonoBehaviour {
+
+    public GameObject Receiver;
+    public Camera Main;
+
+    void Awake() {
+        Transform receiverTransform = Receiver.transform;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 destination = Receiver.transform.position;
+            //Main.transform.position = new Vector3 (10, 4, 0);
+            //Main.transform.Rotate(20, -90, 0);
+            other.transform.position = destination;
+
+        }
+    }
+}
